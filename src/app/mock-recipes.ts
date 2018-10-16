9dcb86a89714fdc9ed0dd1191ff3df50e89f011d
Julianne Soifer and Luke Vandekieft <julianne@>
2018-10-16 13:09:37 -0700

    set up recipe box components.

---
+++ src/app/mock-recipes.ts
@@ -0,0 +1,14 @@
+import { Recipe } from './recipe';
+
+export const RECIPES: Recipe[] = [
+  { title: 'Lasagna', ingredients: ['noodles', 'marinara', 'ricotta'], directions: 'put in oven' },
+  { title: 'Paella', ingredients: ['shrimp', 'rice'], directions: 'cook the rice. This part is important! add the shrimp eventually.'},
+  { title: 'Spaghetti', ingredients: ['noodles', 'meatballs', 'parmesan', 'garlic bread', 'marinara'], directions: 'A very complicated recipe from far off lands!' },
+  { title: 'Lasagna', ingredients: [], directions: 'put in oven' },
+  { title: 'Lasagna', ingredients: [], directions: 'put in oven' },
+  { title: 'Lasagna', ingredients: [], directions: 'put in oven' },
+  { title: 'Lasagna', ingredients: [], directions: 'put in oven' },
+  { title: 'Lasagna', ingredients: [], directions: 'put in oven' },
+  { title: 'Lasagna', ingredients: [], directions: 'put in oven' },
+  { title: 'Lasagna', ingredients: [], directions: 'put in oven' }
+];
